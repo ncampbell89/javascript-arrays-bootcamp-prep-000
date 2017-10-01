993cdc668bb282b568a756978dc279f0123492b1
Nicole Campbell <nscampbell303@gmail.com>
2017-10-01 02:54:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,8 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  array.slice(1)
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
